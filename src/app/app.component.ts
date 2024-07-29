b5baf6fbc83e14109d7a14216b354f90238699eb
eslambashry <114360823+eslambashry@users.noreply.github.com>
2024-07-29 09:37:12 +0300

    Your commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import {UsersComponent} from './users/users.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgbDropdownModule,NgbPaginationModule,UsersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
